a5b0b18629e679aeaca6a2d97f6f971657092872
Harrison Beachey <hbeachey@nd.edu>
2017-02-08 13:52:56 -0500

    adding rough python functionality to add module paths to python path - view ndlib/lambda_purl repo for an example

--- py/hesutil.py
+++ py/hesutil.py
@@ -1,6 +1,11 @@
 import os
+import sys
 
 def getEnv(key, default=None, throw=False):
   if key not in os.environ and throw:
     raise Exception("Key \"%s\" is not in the environment" % key)
   return os.environ.get(key, default)
+
+def addModulePath(base, path):
+  here = os.path.dirname(os.path.relpath(base))
+  sys.path.append(os.path.join(here, path))
