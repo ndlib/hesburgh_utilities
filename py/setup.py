fe68d8d6c0856b3d6d7ac7ec815dbb6aea0566bf
Ryan Doughty <rdought1@nd.edu>
2018-03-21 21:34:15 -0400

    add secret file

--- py/setup.py
+++ py/setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['boto3'],
+    install_requires=['boto3','PyYAML','paramiko'],
 
     # # List additional groups of dependencies here (e.g. development
     # # dependencies). You can install these using the following syntax,
@@ -93,9 +93,9 @@ setup(
     # # To provide executable scripts, use entry points in preference to the
     # # "scripts" keyword. Entry points provide cross-platform support and allow
     # # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    entry_points={
+        'console_scripts': [
+            'secretFiles=hesburgh.secretFiles:main',
+        ],
+    },
 )
